00e943a18c5d166eea889e7bce69960fd4929125
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-10-17 01:14:01 +0100

    create header component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,16 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import Header from '../components/Header'
+
 
 const Home: NextPage = () => {
     return (
-                <div>
+
+        <div className='min-h-screen'>
+            <Header />
+            <div className='mt-10' />
+            {/* <Trending /> */}
+            <div className='mt-20' />
+            {/* <CMCtable /> */}
         </div>
     )
 }
