8b8e990c94c6fa939e1d5ace805918612152ffec
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-10-19 00:13:05 +0100

    make getTopTenCoins accessable globaly

--- context/context.js
+++ context/context.js
@@ -4,10 +4,19 @@ import { createContext, useState, useEffect } from 'react'
 export const TokenUmbrellaContext = createContext()
 
 export const TokenUmbrellaProvider = ({ children }) => {
-
+    const getTopTenCoins = async () => {
+        try {
+            const res = await fetch('/api/getTopTen')
+            const data = await res.json()
+            return data.data.data
+        } catch (e) {
+            console.log(e.message)
+        }
+    }
     return (
         <TokenUmbrellaContext.TokenUmbrellaProvider
             value={{
+                getTopTenCoins
 
             }}
         >
