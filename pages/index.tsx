67cde67f3fb8ff0e4ffffb03940dd8d1896f7e1e
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-10-21 00:22:04 +0100

    create swap container 💯

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
 import type { NextPage } from 'next'
+
+// COMPONENTS
 import Header from '../components/Header'
 import Trending from '../components/Trending'
 import CmcTable from '../components/cmc-table/CmcTable'
+import SwapCryptoModal from '../components/SwapCryptoModal'
 
 
 const Home: NextPage = () => {
@@ -9,6 +12,7 @@ const Home: NextPage = () => {
 
         <div className='min-h-screen'>
             <Header />
+            <SwapCryptoModal />
             <div className='mt-10' />
             <Trending />
             <div className='mt-20' />
